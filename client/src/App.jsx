426c4f71d48a21b923313d96c41e6f0510541f7e
Muzamil2120 <malikmuzamil3889@gmail.com>
2026-06-27 11:27:45 +0500

    code added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,32 @@
-import { Routes, Route } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+import Hero from "../components/Hero/Hero";
+import Deals from "../components/Deals/Deals";
+import CategorySection from "../components/CategorySection/CategorySection";
+import SupplierBanner from "../components/SupplierBanner/SupplierBanner";
+import Recommended from "../components/Recommended/Recommended";
+import Services from "../components/Services/Services";
+import Countries from "../components/Countries/Countries";
+import Newsletter from "../components/Newsletter/Newsletter";
+import Footer from "../components/Footer/Footer";
+
+export default function Home() {
+  return (
+    <div className="bg-[#f7fafc]">
+      <Navbar />
+      <Hero />
+      <Deals />
+      <CategorySection />
+      <SupplierBanner />
+      <Recommended />
+      <Services />
+      <Countries />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
+}
 import Home from "./pages/Home";
 
-function App() {
-
-return (
-
-<Routes>
-
-<Route path="/" element={<Home/>}/>
-
-</Routes>
-
-)
-
+export default function App() {
+  return <Home />;
 }
-
-export default App;
